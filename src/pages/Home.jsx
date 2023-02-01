2cab8c150fa37bf540ae9e67023c6438951c24d1
joao-mororo <joaovitormororo2003@gmail.com>
2023-02-01 13:30:52 -0300

    v1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import MovieCard from '../components/MovieCard'
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
+import '../css/MoviesGrid.css'
+
+const Home = () => {
+    const [topMovies, setTopMovies] = useState([])
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+
+        setTopMovies(data.results)
+    }
+    useEffect(() => {
+        const topRatedURL = `${moviesURL}top_rated?${apiKey}&language=pt-BR`
+
+        getTopRatedMovies(topRatedURL)
+    }, [])
+
+  return (
+    <div className='container'>
+        <h2 className='title'>Melhores filmes</h2>
+        <div className="movies-container">
+            {topMovies === 0 && <p>Carregando...</p>}
+            {topMovies.length > 0 && topMovies.map((movie) => (<MovieCard key={movie.id} movie={movie} />))}
+        </div>
+    </div>
+  )
+}
+
+export default Home
